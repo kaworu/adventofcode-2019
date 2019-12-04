4f9be08372a65e8ff1413c5d3baab422432f5940
Alexandre Perrin <alex@kaworu.ch>
2019-12-04 21:07:08 +0100

    Go !

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# adventofcode-2019
-Advent of Code 2019 puzzles and solutions
+[![Build Status](https://travis-ci.org/kAworu/adventofcode-2019.svg?branch=master)](https://travis-ci.org/kAworu/adventofcode-2019)
+
+# Advent of Code 2019
+
+https://adventofcode.com puzzles solutions in [Go][].
+
+> Advent of Code is a series of small programming puzzles for a variety of
+> skill levels. They are self-contained and are just as appropriate for an
+> expert who wants to stay sharp as they are for a beginner who is just
+> learning to code. Each puzzle calls upon different skills and has two parts
+> that build on a theme.
+
+[Go]: https://golang.org/
---
+++ wake-up.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#
+# Create a directory and generate template files for a given day. Ex:
+#
+#   ./wake-up.sh 01
+
+set -e
+
+ROOT=$(dirname "$0")
+
+if [ $# -ne 1 ]; then
+    echo "usage $(basename "$0") DAY" > /dev/stderr
+    exit 1
+fi
+
+DAY=$1
+
+DAYDIRNAME="day${DAY}"
+DAYDIRPATH="${ROOT}/${DAYDIRNAME}"
+MAINFILENAME="main.go"
+TESTFILENAME="main_test.go"
+MAINFILEPATH="${DAYDIRPATH}/${MAINFILENAME}"
+TESTFILEPATH="${DAYDIRPATH}/${TESTFILENAME}"
+
+if [ -d "${DAYDIRPATH}" ]; then
+    echo "${DAYDIRPATH} already exists" > /dev/stderr
+    exit 1
+fi
+mkdir -p "$DAYDIRPATH"
+
+# input.txt
+: > "${DAYDIRPATH}/input.txt"
+
+# README.md
+: > "${DAYDIRPATH}/README.md"
+
+# README.part2.md
+: > "${DAYDIRPATH}/README.part2.md"
+
+# answer.md
+cat <<EOF > "${DAYDIRPATH}/answer.md"
+Your puzzle answer was \`?\`.
+EOF
+
+# answer.part2.md
+cat <<EOF > "${DAYDIRPATH}/answer.part2.md"
+Your puzzle answer was \`?\`.
+EOF
+
+# Main file
+cat <<EOF > "$MAINFILEPATH"
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+// TODO
+func main() {
+	lines, err := parse(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(lines)
+}
+
+// TODO
+func parse(r io.Reader) ([]string, error) {
+	lines = make([]string, 0)
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+EOF
+
+# Test file
+cat <<EOF > "$TESTFILEPATH"
+package main
+
+import "testing"
+
+func TestMain(t *testing.T) {
+	// TODO
+}
+EOF
