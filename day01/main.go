f645f0d4e312e612173ff84c872dc1e1ca48183c
Alexandre Perrin <alex@kaworu.ch>
2019-12-04 22:44:21 +0100

    Day 1: The Tyranny of the Rocket Equation (part 2)

--- day01/main.go
+++ day01/main.go
@@ -8,15 +8,27 @@ import (
 	"strconv"
 )
 
-type (
-	Mass   int  // The mass of something.
-	Module Mass // A module to be launched.
-	Fuel   Mass // Required to launch modules.
-)
+// The weight of something.
+type Mass int
 
-// Return the amount of Fuel required to launch this Module.
-func (mod Module) FuelRequired() Fuel {
-	return Fuel(mod/3 - 2)
+// Returns the amount of fuel required to carry the given mass.
+func FuelRequired(m Mass) Mass {
+	return m/3 - 2
+}
+
+// Return the amount of fuel required to launch all the given modules, along
+// with the amount of fuel required for the fuel.
+func TotalFuelRequired(modules ...Mass) (mf, ff Mass) {
+	for _, module := range modules {
+		fuel := FuelRequired(module)
+		mf += fuel
+		fuel = FuelRequired(fuel)
+		for fuel > 0 {
+			ff += fuel
+			fuel = FuelRequired(fuel)
+		}
+	}
+	return
 }
 
 // Compute and display the sum of the fuel requirements for all of the modules
@@ -26,16 +38,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	total := Fuel(0)
-	for _, mod := range modules {
-		total += mod.FuelRequired()
-	}
-	fmt.Printf("The sum of the fuel requirements is %d.\n", total)
+	mf, ff := TotalFuelRequired(modules...)
+	fmt.Printf("The sum of the fuel requirements is %d,\n", mf)
+	fmt.Printf("and when also taking into account the mass of the added fuel it is %d.\n", mf+ff)
 }
 
-// Parse one Module per line of input.
-func parse(r io.Reader) ([]Module, error) {
-	modules := make([]Module, 0)
+// Parse one module per line of input.
+func parse(r io.Reader) ([]Mass, error) {
+	modules := make([]Mass, 0)
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -43,7 +53,7 @@ func parse(r io.Reader) ([]Module, error) {
 		if err != nil {
 			return nil, err
 		}
-		modules = append(modules, Module(i))
+		modules = append(modules, Mass(i))
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, err
--- day01/main_test.go
+++ day01/main_test.go
@@ -6,9 +6,8 @@ func TestFuelRequired(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		Name string
-		Mod  Module
-		Exp  Fuel
+		Name     string
+		Mod, Exp Mass
 	}{
 		{"first example", 12, 2},
 		{"second example", 14, 2},
@@ -20,8 +19,32 @@ func TestFuelRequired(t *testing.T) {
 		tc := tc // capture range variable
 		t.Run(tc.Name, func(t *testing.T) {
 			t.Parallel()
-			if r := tc.Mod.FuelRequired(); r != tc.Exp {
+			if r := FuelRequired(Mass(tc.Mod)); r != tc.Exp {
 				t.Errorf("%v.FuelRequired() = %d, expected %d", tc.Mod, r, tc.Exp)
+			}
+		})
+	}
+}
+
+func TestTotalFuelRequired(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		Name     string
+		Mod, Exp Mass
+	}{
+		{"first example", 14, 2},
+		{"second example", 1969, 966},
+		{"third example", 100756, 50346},
+	}
+
+	for _, tc := range tests {
+		tc := tc // capture range variable
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			mf, ff := TotalFuelRequired(tc.Mod)
+			if r := mf + ff; r != tc.Exp {
+				t.Errorf("TotalFuelRequired(%v) = %d, expected %d", tc.Mod, r, tc.Exp)
 			}
 		})
 	}
