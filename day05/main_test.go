7115e1e27fff153cf45eddee5470d64570bf40bc
Alexandre Perrin <alex@kaworu.ch>
2019-12-16 11:36:38 +0100

    Day 5: Sunny with a Chance of Asteroids (part 2)
    Not the prettiest but that'll do for now.

--- day05/main_test.go
+++ day05/main_test.go
@@ -2,34 +2,18 @@ package main
 
 import "testing"
 
-func TestExecute(t *testing.T) {
+func TestInputOutput(t *testing.T) {
 	tests := []struct {
-		Name       string
-		Program    Memory
-		Input      Input
-		Expected   Output
-		FinalState Memory
+		Name     string
+		Program  Memory
+		Input    Input
+		Expected Output
 	}{
 		{
-			Name:       "identity program",
-			Program:    Memory{3, 0, 4, 0, 99},
-			Input:      Input{42},
-			Expected:   Output{42},
-			FinalState: Memory{42, 0, 4, 0, 99},
-		},
-		{
-			Name:       "multiply then halt",
-			Program:    Memory{1002, 4, 3, 4, 33},
-			Input:      nil,
-			Expected:   nil,
-			FinalState: Memory{1002, 4, 3, 4, 99},
-		},
-		{
-			Name:       "add immediate with negative value",
-			Program:    Memory{1101, 100, -1, 4, 0},
-			Input:      nil,
-			Expected:   nil,
-			FinalState: Memory{1101, 100, -1, 4, 99},
+			Name:     "identity program",
+			Program:  Memory{3, 0, 4, 0, 99},
+			Input:    Input{42},
+			Expected: Output{42},
 		},
 	}
 
@@ -37,14 +21,103 @@ func TestExecute(t *testing.T) {
 		t.Run(tc.Name, func(t *testing.T) {
 			mem := tc.Program.Copy()
 			output, err := mem.Execute(tc.Input)
-			if err != nil {
+			switch {
+			case err != nil:
 				t.Errorf("Unexpected execute error: %s", err)
-			}
-			if !IntcodeEqual(output, tc.Expected) {
+			case !IntcodeEqual(output, tc.Expected):
 				t.Errorf("expected %v as output, got %v", tc.Expected, output)
 			}
-			if !IntcodeEqual(mem, tc.FinalState) {
+		})
+	}
+}
+
+func TestModes(t *testing.T) {
+	tests := []struct {
+		Name       string
+		Program    Memory
+		FinalState Memory
+	}{
+		{
+			Name:       "multiply then halt",
+			Program:    Memory{1002, 4, 3, 4, 33},
+			FinalState: Memory{1002, 4, 3, 4, 99},
+		},
+		{
+			Name:       "add immediate with negative value",
+			Program:    Memory{1101, 100, -1, 4, 0},
+			FinalState: Memory{1101, 100, -1, 4, 99},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			mem := tc.Program.Copy()
+			_, err := mem.Execute( /* Input */ nil)
+			switch {
+			case err != nil:
+				t.Errorf("Unexpected execute error: %s", err)
+			case !IntcodeEqual(mem, tc.FinalState):
 				t.Errorf("expected %v as final state, got %v", tc.FinalState, mem)
+			}
+		})
+	}
+}
+
+func TestComparisons(t *testing.T) {
+	eq8 := func(in Input) Output {
+		if in[0] == 8 {
+			return Output{1}
+		}
+		return Output{0}
+	}
+	lt8 := func(in Input) Output {
+		if in[0] < 8 {
+			return Output{1}
+		}
+		return Output{0}
+	}
+	tests := []struct {
+		Name    string
+		Program Memory
+		Execute func(in Input) Output
+	}{
+		{
+			Name:    "position mode input==8",
+			Program: Memory{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8},
+			Execute: eq8,
+		},
+		{
+			Name:    "position mode input<8",
+			Program: Memory{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8},
+			Execute: lt8,
+		},
+		{
+			Name:    "immediate mode input==8",
+			Program: Memory{3, 3, 1108, -1, 8, 3, 4, 3, 99},
+			Execute: eq8,
+		},
+		{
+			Name:    "immediate mode input<8",
+			Program: Memory{3, 3, 1107, -1, 8, 3, 4, 3, 99},
+			Execute: lt8,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			for i := -10; i <= 10; i++ {
+				mem := tc.Program.Copy()
+				in := Input{Intcode(i)}
+				expected := tc.Execute(in)
+				output, err := mem.Execute(in)
+				switch {
+				case err != nil:
+					t.Errorf("input %d: Unexpected execute error: %s", i, err)
+					return
+				case !IntcodeEqual(output, expected):
+					t.Errorf("input %d: expected %v as output, got %v", i, expected, output)
+					return
+				}
 			}
 		})
 	}
