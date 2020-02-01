5d98e05dfd3172b3fdd5a76744cfbfb5f5c25dbb
Alexandre Perrin <alex@kaworu.ch>
2020-02-01 09:14:02 +0100

    Day 7: Amplification Circuit (part 1)

---
+++ day07/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import "testing"
+
+var tests = []struct {
+	Name     string
+	Program  Memory
+	Sequence []Intcode
+	Expected Intcode
+}{
+	{
+		Name:     "first example",
+		Program:  Memory{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+		Sequence: []Intcode{4, 3, 2, 1, 0},
+		Expected: 43210,
+	},
+	{
+		Name:     "second example",
+		Program:  Memory{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+		Sequence: []Intcode{0, 1, 2, 3, 4},
+		Expected: 54321,
+	},
+	{
+		Name:     "third example",
+		Program:  Memory{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+		Sequence: []Intcode{1, 0, 4, 3, 2},
+		Expected: 65210,
+	},
+}
+
+func TestSeries(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			output := Series(tc.Program, tc.Sequence)
+			if output != tc.Expected {
+				t.Errorf("expected %v as output, got %v", tc.Expected, output)
+			}
+		})
+	}
+}
+
+func TestHighestSignal(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.Name, func(t *testing.T) {
+			output := HighestSignal(tc.Program, []Intcode{0, 1, 2, 3, 4})
+			if output != tc.Expected {
+				t.Errorf("expected %v as output, got %v", tc.Expected, output)
+			}
+		})
+	}
+}
